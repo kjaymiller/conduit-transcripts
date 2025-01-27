316022b21b3741fd70f0bace9ddbd58a5fdb1722
Jay Miller <kjaymiller@gmail.com>
2025-01-27 07:44:33 -0500

    removed chunking from transcriptions

--- tools/transcribe.py
+++ tools/transcribe.py
@@ -3,68 +3,40 @@
 import pathlib
 import tempfile
 import typing
+import typing_extensions
 
-from rich.progress import track 
 import frontmatter
 import httpx
 import slugify
 import typer
 import whisper
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-
-from url_finder import get_audio_url_from_episode_number, fetch_latest_episode_number
+from rich.progress import track
+from rich.prompt import Confirm
+from url_finder import fetch_latest_episode_number, get_audio_url_from_episode_number
 
 app = typer.Typer()
 
-model = whisper.load_model("base")    
+model = whisper.load_model("base")
 
 
 splitter = RecursiveCharacterTextSplitter(
     chunk_size=300,
-    chunk_overlap=20,
     separators=[".", "!", "?", "\n"],
+    keep_separator="end",
+    chunk_overlap=0,
 )
 
-
-def check_first_character(segment: str) -> bool:
-    """Check if a segment needs to have the first character removed"""
-    return segment[0] in (".", "!", "?")
-
-
-def split_text(text: str) -> list[str]:
-    """
-    Split a block of text into paragraphs.
-    
-    Text is written in a single string and split using the RecursiveCharacterTextSplitter.
-
-    This corrects for text leading with punctuation will grab the punctuation from the next segment.
-    """
-    text_segments = splitter.split_text(text)
-    new_text_segments = []
-    
-    for i, segment in enumerate(text_segments):
-        if check_first_character(segment):
-            modified_new_text_segment = segment[1:].lstrip()
-        else:
-            modified_new_text_segment = segment
-
-        if segment != text_segments[-1]:
-            if check_first_character(text_segments[i+1]):
-                modified_new_text_segment += text_segments[i + 1][1:]
-        
-        new_text_segments.append(modified_new_text_segment)
-
-    return new_text_segments
 
 def download_audio_file(url: str) -> str:
     """
     Stream Download an audio file from a URL.
-    
+
     Save the audio file to a temporary file.
 
     Args:
         url: The URL of the audio file.
-    
+
     Returns:
         The path to the downloaded audio file.
     """
@@ -73,7 +45,7 @@ def download_audio_file(url: str) -> str:
         typer.echo(f"Downloading audio file from {url}")
 
         with httpx.stream("GET", url, follow_redirects=True) as response:
-        
+
             typer.echo(f"Saving audio file to {f.name}")
             for chunk in response.iter_bytes():
                 f.write(chunk)
@@ -84,8 +56,7 @@ def download_audio_file(url: str) -> str:
 def transcribe_audio_file(audio_file: pathlib.Path) -> str:
     """Transcribe an audio file to text"""
 
-    transcription = model.transcribe(audio=audio_file, verbose=False)
-    return "\n".join(split_text(transcription["text"]))
+    return model.transcribe(audio=audio_file, verbose=False)["text"]
 
 
 def transcribe_from_audio_url(audio_url: str) -> int:
@@ -94,7 +65,18 @@ def transcribe_from_audio_url(audio_url: str) -> int:
     return transcribe_audio_file(audio_file)
 
 
-def transcribe_from_episode_number(episode_numbers: typing.Optional[typing.List[int]]=None):
+def transcribe_from_episode_number(
+    episode_numbers: typing.Optional[typing.List[int]] = None,
+    all: typing_extensions.Annotated[
+        bool, typer.Option("-a", "--all", help="redownload_all_files")
+    ] = False,
+    _range: typing_extensions.Annotated[
+        typing.Optional[str],
+        typer.Option(
+            "-r", "--range", help="two numbers separated by a dash ('-'). Example: 1-10"
+        ),
+    ] = None,
+):
     """
     Transcribe an episode from an episode number
 
@@ -103,17 +85,32 @@ def transcribe_from_episode_number(episode_numbers: typing.Optional[typing.List[
     Audio is downloaded from the extracted audio_url.
     """
 
-    if not episode_numbers:
+    if all:
+        if Confirm.ask(
+            "This Process will re-download and overwrite all transcriptions. Continue?",
+            show_choices=True,
+            default=False,
+        ):
+            episode_numbers = range(1, fetch_latest_episode_number())
+
+    elif _range:
+        start, stop = _range.split("-")
+        episode_numbers = range(int(start), int(stop) + 1)
+
+    elif not episode_numbers:
         typer.echo("Fetching Latest Episode Number")
         episode_numbers = [fetch_latest_episode_number()]
 
     for episode_number in track(episode_numbers):
         metadata, audio_url = get_audio_url_from_episode_number(episode_number)
         transcription = transcribe_from_audio_url(audio_url)
-        post = frontmatter.Post(transcription, **metadata)
-        output_file = pathlib.Path(f"transcripts/{slugify.slugify(metadata['title'])}.md")
+        post = frontmatter.Post(
+            "\n".join(splitter.split_text(transcription)), **metadata
+        )
+        output_file = pathlib.Path(
+            f"transcripts/{slugify.slugify(metadata['title'])}.md"
+        )
         output_file.write_text(frontmatter.dumps(post))
-
 
 
 if __name__ == "__main__":
